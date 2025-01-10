6f1477ea371a6dcecff8427510748d16634da8b9
Sophie Wheeler <swheeler@skedulo.com>
2025-01-10 10:37:21 +0700

    add public page example

---
+++ public-page/example-compiled-page/src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import imgUrl from "./assets/ShiftSolarLogo.png"
+
+function App() {
+  return (<>
+    <img src={imgUrl} alt="Shift Solar" />
+    <h1>Hello, world!</h1>
+  </>)
+}
+
+export default App;
